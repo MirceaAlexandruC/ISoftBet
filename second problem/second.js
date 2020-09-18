ceeec08921a3531ce1d18728f154383257cead6b
cioatamircea@gmail.com <cioatamircea:gmail.com>
2020-09-18 16:12:06 +0300

    update

---
+++ second problem/second.js
@@ -0,0 +1,23 @@
+function onError() {
+    document.getElementById("error").style.display = "block";
+  }
+  
+  function offError() {
+    document.getElementById("error").style.display = "none";
+  }
+  
+  function onLeave() {
+    document.getElementById("leave").style.display = "block";
+  }
+  
+  function offLeave() {
+    document.getElementById("leave").style.display = "none";
+  }
+  
+  function onMenu() {
+    document.getElementById("menu").style.display = "block";
+  }
+  
+  function offMenu() {
+    document.getElementById("menu").style.display = "none";
+  }
